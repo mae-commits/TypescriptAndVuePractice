61dccd6bf441d3d2d9b928eb3e1b4937b3bf6b08
maekawa <himahimastudent@gmail.com>
2022-09-06 23:37:25 +0900

    Vue.js  Practice

--- vueSample/vueSample/src/App.vue
+++ vueSample/vueSample/src/App.vue
@@ -1,9 +1,108 @@
+<template>
+  <Form />
+</template>
+
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-// import payment from './components/payment.vue';
-// import Tweet from './components/Tweet.vue';
+import Form from './components/Form.vue';
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
+}
+</style>
+
+<!-- <script setup lang="ts">
+import {provide} from 'vue'
+import {todos, todoKey} from  './useTodo'
+import TodoList from './components/TodoList.vue';
+
+provide(todoKey, todos)
+</script>
+
+<template>
+  <TodoList />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style> -->
+
+<!-- <script setup lang="ts">
+import { ref, computed} from 'vue'
+import CardList from './components/CardList.vue';
+import SecondCardList from './components/SecondCardList.vue';
+
+const isFirstTab = ref(true)
+const updateTab = (isFirst: boolean) => {
+  isFirstTab.value = isFirst
+}
+
+const isActiveTabName = ref('1')
+
+const updateTab = (activeName: string) => {
+  isActiveTabName.value = activeName
+}
+const currentComponent = computed(() => {
+  switch(isActiveTabName.value) {
+    case '1':
+      return CardList
+    case '2':
+      return SecondCardList
+    case '3':
+      return CardList
+    default:
+      return CardList
+  }
+})
+</script> -->
+  
+  <!-- <template>
+    <div class="tab-changer">
+      <button @click="updateTab('1')">Tab 1</button>
+      <button @click="updateTab('2')">Tab 2</button>
+      <button @click="updateTab('3')">Tab 3</button>
+    </div>
+    <div class="tab-contents">
+      <CardList></CardList>
+      <component :is="currentComponent"></component>
+    </div>
+  </template> -->
+  
+  <!-- <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  
+  .tab-contents {
+    margin-top: 20px;
+  
+  }
+  </style> -->
+  
+<!-- <script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import payment from './components/payment.vue';
+import Tweet from './components/Tweet.vue';
 import Person from './components/Person.vue'
 </script>
 
@@ -11,7 +110,7 @@ import Person from './components/Person.vue'
   <Person />
 </template>
 
-<!-- <style>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,3 +120,6 @@ import Person from './components/Person.vue'
   margin-top: 60px;
 }
 </style> -->
+
+<!-- // This starter template is using Vue 3 <script setup> SFCs -->
+<!-- // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup -->
---
+++ vueSample/vueSample/src/components/Reservation.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import Card from './Card.vue'
+import Badge from './Badge.vue'
+import MosaicReservation from './MosaicReservation.vue';
+import ReservationList from './ReservationList.vue';
+
+const isListVisible = ref<boolean>(true)
+
+const isDialogOpen = ref<boolean>(false)
+const onClickButton = () => {
+    // isListVisible.value = !isListVisible.value
+    isDialogOpen.value = !isDialogOpen.value
+}
+
+const getContent = () => {
+    if (isListVisible.value) {
+        return ReservationList
+    } else {
+        return MosaicReservation
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <Card class="menu-card">
+            <span>Badges</span>
+            <div class="badges">
+                <Badge class="vip-badge">
+                    <span>VIP</span>
+                </Badge>
+                <Badge class="normal-badge">
+                    <span>NORMAL</span>
+                </Badge>
+            </div>
+        </Card>
+        <component :is="getContent()" />
+        <button @click="onClickButton">Change</button>
+        <dialog class="dialog" :open="isDialogOpen">
+            <span>Dialog</span>
+        </dialog>
+    </div>
+
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.dialog {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 300px;
+    width: 400px;
+    margin: auto;
+    background-color: aliceblue;
+}
+</style>
